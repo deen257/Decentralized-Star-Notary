61eb1730ae9562ff59558222c20c30b07b3add6c
ayomide <folorunshoayomide46@gmail.com>
2022-11-18 21:44:12 +0100

    migration to goerli and nft mining

--- truffle-config.js
+++ truffle-config.js
@@ -22,72 +22,31 @@
  *
  */
 
-// require('dotenv').config()
-// const { mnemonic, infuraKey } = process.env
+require('dotenv').config()
+const { mnemonic, infuraKey } = process.env
 
-
-
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-
+const HDWalletProvider = require('truffle-hdwallet-provider')
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
   networks: {
-    // development: {
-    //   host: '127.0.0.1', // Localhost (default: none)
-    //   port: 9545, // Standard Ethereum port (default: none)
-    //   network_id: '*', // Any network (default: none)
-    // },
-    // // Useful for deploying to a public network.
-    // // NB: It's important to wrap the provider as a function.
-    // goerli: {
-    //   provider: () =>
-    //     new HDWalletProvider(
-    //       mnemonic,
-    //       `https://goerli.infura.io/v3/${infuraKey}`
-    //     ),
-    //   network_id: 5, // Goerli's id
-    //   confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
-    // },
-
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-
+    development: {
+      host: '127.0.0.1', // Localhost (default: none)
+      port: 9545, // Standard Ethereum port (default: none)
+      network_id: '*', // Any network (default: none)
+    },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
+    goerli: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://goerli.infura.io/v3/${infuraKey}`
+        ),
+      network_id: 5, // Goerli's id
+      confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
   },
 
   // Set default mocha options here, use special reporters etc.
